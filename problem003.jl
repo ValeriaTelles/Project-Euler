805e7dc15a68e35b1ab9d12ff9cbda728bbfd72a
Valeria TC <51431966+ValeriaTelles@users.noreply.github.com>
2023-05-08 14:43:24 -0400

    Solution to Problem 3

--- problem003.jl
+++ problem003.jl
@@ -4,7 +4,6 @@ module problem003
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 """
-
 function largest_prime_factor(n, factor)
     while n > factor
         if n % factor == 0
