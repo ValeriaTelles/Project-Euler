bcb96316357eee4d24895e4431d2be9ad8b12eee
Valeria TC <51431966+ValeriaTelles@users.noreply.github.com>
2023-05-09 13:23:44 -0400

    Solution to Problem 5

---
+++ problem005.jl
@@ -0,0 +1,10 @@
+module problem005
+
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+smallest_multiple = lcm(1:20)
+println(smallest_multiple)
+
+end
